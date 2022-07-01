831f70015965cc3a59d6b2f806ab928f9a0d30f2
gsierra22 <giancarlosierra2011@hotmail.com>
2022-07-01 11:58:29 -0500

    working movie links

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,46 +1,50 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpClient} from '@angular/common/http'
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  trendingMovies: any;
+  theatreMovies: any;
+  popularMovies: any;
 
-  trendingMovies: any
-  theatreMovies: any
-  popularMovies:any
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) {}
 
   ngOnInit(): void {
-    this.getTrendingMovies()
-    this.getTheatreMovies()
-    this.getPopularMovies()
-  }
-  getTrendingMovies(){
-    this.http.get('http://localhost:4200/assets/data/trending-movies.json').subscribe((movies) =>{
-      this.trendingMovies = movies;
-      console.log(this.trendingMovies)
-    });
-
+    this.getTrendingMovies();
+    this.getTheatreMovies();
+    this.getPopularMovies();
   }
 
-  getTheatreMovies(){
-    this.http.get('http://localhost:4200/assets/data/theatre-movies.json').
-    subscribe((movies) =>{
-      this.theatreMovies = movies;
-      console.log(this.theatreMovies)
-    });
-
-  }
-  getPopularMovies(){
-    this.http.get('http://localhost:4200/assets/data/popular-movies.json').
-    subscribe((movies) =>{
-      this.popularMovies = movies;
-      console.log(this.popularMovies)
-    });
+  getTrendingMovies() {
+    this.http
+      .get('http://localhost:4200/assets/data/trending-movies.json')
+      .subscribe((movies) => {
+        this.trendingMovies = movies;
+      });
   }
 
+  getTheatreMovies() {
+    this.http
+      .get('http://localhost:4200/assets/data/theatre-movies.json')
+      .subscribe((movies) => {
+        this.theatreMovies = movies;
+      });
+  }
+
+  getPopularMovies() {
+    this.http
+      .get('http://localhost:4200/assets/data/popular-movies.json')
+      .subscribe((movies) => {
+        this.popularMovies = movies;
+      });
+  }
+
+  goToMovie(type: string, id: string) {
+    this.router.navigate(['movie', type, id]);
+  }
 }
